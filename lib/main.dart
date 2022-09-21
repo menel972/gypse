4773fe0ac98110767212515ef06dca03fa6959b9
menel972 <m.grandisson@hotmail.com>
2022-09-21 09:05:37 +0200

    Configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:gypse/core/commons/current_user.dart';
@@ -6,11 +7,18 @@ import 'package:gypse/core/themes/theme.dart';
 import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   /// Preserves the splash on screen until the method [FlutterNativeSplash.remove()] is invoked
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  /// Initialize Firebase from [firebase_core.dart] with the configuration of [firebase_options.dart]
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   /// Start app
   runApp(const MyApp());

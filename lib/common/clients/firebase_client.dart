2e485dd1e2144a9dc0887c3e2bde26437e795733
Menelick Grandisson <mgrandisson@exomind.fr>
2023-04-12 15:11:59 +0200

    FEAT : creates user service

--- lib/common/clients/firebase_client.dart
+++ lib/common/clients/firebase_client.dart
@@ -13,19 +13,19 @@ class FirebaseClients {
   ///## Questions database
   ///
   ///An instance of the collection in the Firebase Firestore database <b>where questions are stored</b>.
-  final CollectionReference<Map<String, dynamic>> questionsDb =
+  static final CollectionReference<Map<String, dynamic>> questionsDb =
       FirebaseFirestore.instance.collection('question');
 
   ///## Answers database
   ///
   ///An instance of the collection in the Firebase Firestore database <b>where answers are stored</b>.
-  final CollectionReference<Map<String, dynamic>> answersDb =
+  static final CollectionReference<Map<String, dynamic>> answersDb =
       FirebaseFirestore.instance.collection('reponse');
 
   ///## Users database
   ///
   ///An instance of the collection in the Firebase Firestore database <b>where users are stored</b>.
-  final CollectionReference<Map<String, dynamic>> usersDb =
+  static final CollectionReference<Map<String, dynamic>> usersDb =
       FirebaseFirestore.instance.collection('user');
 
   ///## Authentication client

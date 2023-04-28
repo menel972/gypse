a67ab7f48ad5a7a47ac1b7472fb13c63f296fe29
Menelick Grandisson <mgrandisson@exomind.fr>
2023-04-28 11:25:50 +0200

    FEAT : auth screen

---
+++ lib/auth/presentation/views/widgets/forgotten_password_view.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gypse/auth/domain/repositories/auth_repository.dart';
+import 'package:gypse/auth/presentation/views/states/auth_credentials_state.dart';
+import 'package:gypse/auth/presentation/views/states/forgotten_password_bloc.dart';
+import 'package:gypse/common/style/buttons.dart';
+import 'package:gypse/common/style/fonts.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/extensions.dart';
+import 'package:gypse/core/l10n/localizations.dart';
+
+class ForgottenPasswordView extends StatelessWidget {
+  final Function(int) onSelectedView;
+  final AuthRepository auth;
+  const ForgottenPasswordView(this.onSelectedView,
+      {required this.auth, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ForgottenPasswordBloc, AuthCredentialsState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Dimensions.s(context).spaceH(),
+              Text(
+                words(context).title_forget_mdp,
+                style: GypseFont.xl(bold: true),
+                textAlign: TextAlign.center,
+                maxLines: 2,
+              ),
+              Dimensions.xxs(context).spaceH(),
+              Text(
+                words(context).txt_forget_mdp,
+                style: GypseFont.m(bold: true),
+                textAlign: TextAlign.center,
+                maxLines: 2,
+              ),
+              Dimensions.xs(context).spaceH(),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: words(context).label_mail,
+                  suffixIcon: Icons.alternate_email.show(),
+                ),
+                textInputAction: TextInputAction.done,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) =>
+                    context.read<ForgottenPasswordBloc>().onEmailChanged(value),
+              ),
+              Visibility(
+                visible: !state.emailError.isNull || state.emailError != '',
+                child: Text(
+                  state.emailError ?? '',
+                  style:
+                      GypseFont.xs(color: Theme.of(context).colorScheme.error),
+                  maxLines: 1,
+                ),
+              ),
+              Dimensions.xxs(context).spaceH(),
+              Row(
+                children: [
+                  Expanded(
+                    child: GypseElevatedButton(
+                      context,
+                      onPressed: () async {
+                        bool isFormValid = context
+                            .read<ForgottenPasswordBloc>()
+                            .onFormValidated();
+
+                        if (isFormValid) {
+                          await auth
+                              .forgottenPassword(state.email)
+                              .then((bool? result) {
+                            if (result == true) {
+                              words(context).txt_mail.success(context);
+                              return result;
+                            }
+                          }).catchError((e) {
+                            String msg = e.message as String;
+                            msg.failure(context);
+                            msg.log();
+                            return false;
+                          });
+                        } else {
+                          words(context).snack_error_form.failure(context);
+                        }
+                      },
+                      label: words(context).btn_send,
+                      textColor: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                  Dimensions.xxs(context).spaceW(),
+                  Expanded(
+                    child: GypseElevatedButton(
+                      context,
+                      onPressed: () => onSelectedView(1),
+                      label: words(context).btn_annule,
+                      textColor: Theme.of(context).colorScheme.onPrimary,
+                      backgroundColor: Theme.of(context)
+                          .colorScheme
+                          .onPrimary
+                          .withOpacity(0.2),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/auth/presentation/views/widgets/sign_up_view.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gypse/auth/domain/models/auth_request.dart';
+import 'package:gypse/auth/domain/repositories/auth_repository.dart';
+import 'package:gypse/auth/presentation/views/states/auth_credentials_state.dart';
+import 'package:gypse/auth/presentation/views/states/sign_up_bloc.dart';
+import 'package:gypse/common/style/buttons.dart';
+import 'package:gypse/common/style/fonts.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/enums.dart';
+import 'package:gypse/common/utils/extensions.dart';
+import 'package:gypse/core/l10n/localizations.dart';
+
+class SignUpView extends StatelessWidget {
+  final AuthRepository auth;
+  const SignUpView(this.auth, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SignUpBloc, AuthCredentialsState>(
+      listener: (context, state) {
+        switch (state.connectionStatus) {
+          case LoginState.loading:
+            'LOADING'.log();
+            break;
+          case LoginState.authenticated:
+            'AUTHENTICATED'.log();
+            break;
+          case LoginState.unauthenticated:
+            'UNAUTHENTICATED'.log();
+            break;
+          default:
+            break;
+        }
+      },
+      builder: (context, state) {
+        // NOTE : Set Connection State
+        authTrue() => context
+            .read<SignUpBloc>()
+            .onConnectionStatusChanged(LoginState.authenticated);
+        authFalse() => context
+            .read<SignUpBloc>()
+            .onConnectionStatusChanged(LoginState.unauthenticated);
+        authLoad() => context
+            .read<SignUpBloc>()
+            .onConnectionStatusChanged(LoginState.loading);
+
+        // NOTE : RETURN
+        return SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Dimensions.xxs(context).spaceH(),
+              Text(
+                words(context).btn_signin,
+                style: GypseFont.xxl(bold: true),
+                textAlign: TextAlign.center,
+              ),
+              Dimensions.xs(context).spaceH(),
+              // NOTE : USER NAME
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: words(context).label_name,
+                  suffixIcon: Icons.person_outline.show(),
+                ),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.name,
+                onChanged: (value) =>
+                    context.read<SignUpBloc>().onUserNameChanged(value),
+              ),
+              Visibility(
+                visible:
+                    !state.userNameError.isNull || state.userNameError != '',
+                child: Text(
+                  state.userNameError ?? '',
+                  style:
+                      GypseFont.xs(color: Theme.of(context).colorScheme.error),
+                  maxLines: 1,
+                ),
+              ),
+              Dimensions.xxxs(context).spaceH(),
+              // NOTE : EMAIL
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: words(context).label_mail,
+                  suffixIcon: Icons.alternate_email.show(),
+                ),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) =>
+                    context.read<SignUpBloc>().onEmailChanged(value),
+              ),
+              Visibility(
+                visible: !state.emailError.isNull || state.emailError != '',
+                child: Text(
+                  state.emailError ?? '',
+                  style:
+                      GypseFont.xs(color: Theme.of(context).colorScheme.error),
+                  maxLines: 1,
+                ),
+              ),
+              Dimensions.xxxs(context).spaceH(),
+              // NOTE : PASSWORD
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: words(context).label_mdp,
+                  suffixIcon: IconButton(
+                    onPressed: () => context
+                        .read<SignUpBloc>()
+                        .onPasswordVisibilityChanged(),
+                    icon: state.isPasswordHidden
+                        ? Icons.remove_red_eye_outlined.show()
+                        : Icons.visibility_off_outlined.show(),
+                  ),
+                ),
+                textInputAction: TextInputAction.done,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: state.isPasswordHidden,
+                onChanged: (value) =>
+                    context.read<SignUpBloc>().onPasswordChanged(value),
+              ),
+              Visibility(
+                visible:
+                    !state.passwordError.isNull || state.passwordError != '',
+                child: Text(
+                  state.passwordError ?? '',
+                  style:
+                      GypseFont.xs(color: Theme.of(context).colorScheme.error),
+                  maxLines: 1,
+                ),
+              ),
+              Dimensions.xs(context).spaceH(),
+              // NOTE : SIGNUP BUTTON
+              GypseElevatedButton(
+                context,
+                onPressed: () async {
+                  bool isFormValid =
+                      context.read<SignUpBloc>().onSignUpRequest();
+
+                  if (isFormValid) {
+                    authLoad();
+                    // NOTE : Try to signup
+                    await auth
+                        .signUp(AuthRequest(state.email, state.password))
+                        // NOTE : Case : SUCCESS
+                        .then((String result) {
+                      if (!result.isEmpty) {
+                        authTrue();
+                        '${words(context).snack_welcome} ${state.userName}'
+                            .success(context);
+                        return result;
+                      }
+                    })
+                        // NOTE : Case : ERROR
+                        .catchError((e) {
+                      String msg = e.message as String;
+                      authFalse();
+                      msg.failure(context);
+                      msg.log();
+                      return '';
+                    });
+                  } else {
+                    words(context).snack_error_form.failure(context);
+                    authFalse();
+                  }
+                },
+                label: words(context).btn_signin,
+                textColor: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}

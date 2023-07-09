41f90f1811130b5be9ba8fb06d4559fe15cbfb30
Menelick Grandisson <mgrandisson@exomind.fr>
2023-07-09 09:28:45 +0200

    FEAT : creates home state

---
+++ lib/home/presentation/views/states/home_navigation_state.dart
@@ -0,0 +1,16 @@
+import 'package:gypse/common/utils/extensions.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HomeNavigationState extends StateNotifier<int> {
+  HomeNavigationState() : super(0);
+
+  void updatePage(int index) {
+    state = index;
+    state.log(tag: 'Current Page');
+  }
+}
+
+final homeNavigationStateProvider =
+    StateNotifierProvider.autoDispose<HomeNavigationState, int>((ref) {
+  return HomeNavigationState();
+});

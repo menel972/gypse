0848ab42452840d9842fb161acc7102e6492bd80
Menelick Grandisson <mgrandisson@exomind.fr>
2023-07-10 16:02:30 +0200

    FEAT : creates credentials view

---
+++ lib/home/presentation/views/widgets/profile/dialogs/delete_account_dialog.dart
@@ -0,0 +1,114 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:blur/blur.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gypse/auth/domain/usecase/auth_use_cases.dart';
+import 'package:gypse/auth/domain/usecase/user_use_case.dart';
+import 'package:gypse/common/providers/user_provider.dart';
+import 'package:gypse/common/style/buttons.dart';
+import 'package:gypse/common/style/fonts.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/enums.dart';
+import 'package:gypse/common/utils/extensions.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class DeleteAccountDialog extends HookConsumerWidget {
+  final BuildContext context;
+
+  late String? uId;
+  DeleteAccountDialog(this.context, {super.key}) {
+    showDialog(
+      context: context,
+      builder: (context) => this,
+    );
+  }
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    uId = ref.watch(userProvider)?.uId;
+    Future<bool> deleteAccountUseCase() =>
+        ref.read(deleteAccountUseCaseProvider).invoke();
+    Future<void> onDeleteUserUseCase() =>
+        ref.read(onDeleteUserUseCaseProvider).invoke(context, uId!);
+
+    return Center(
+      child: Container(
+        height: Dimensions.xl(context).height,
+        width: Dimensions.screen(context).width * 0.9,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface.withOpacity(0.4),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Theme.of(context).colorScheme.surface),
+        ),
+        child: Blur(
+          blur: 3,
+          blurColor: Theme.of(context).colorScheme.surface,
+          borderRadius: BorderRadius.circular(20),
+          overlay: Padding(
+            padding: EdgeInsets.all(Dimensions.xxs(context).height),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'SUPPRESSION DU PROFILE',
+                  style: GypseFont.l(
+                    bold: true,
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+                  textAlign: TextAlign.center,
+                  maxLines: 2,
+                ),
+                Dimensions.xs(context).spaceH(),
+                Text(
+                  'Êtes-vous sûr.e de vouloir supprimer votre profile GYPSE. Cette action est définitive.',
+                  style:
+                      GypseFont.m(color: Theme.of(context).colorScheme.primary),
+                  textAlign: TextAlign.center,
+                  maxLines: 3,
+                ),
+                Dimensions.xs(context).spaceH(),
+                Row(
+                  children: [
+                    Expanded(
+                      child: GypseElevatedButton(
+                        context,
+                        label: 'Supprimer',
+                        onPressed: () async {
+                          bool result = await deleteAccountUseCase();
+
+                          if (result) {
+                            await onDeleteUserUseCase().whenComplete(
+                                () => context.go(Screen.authView.path));
+                            ;
+                          } else {
+                            'Une erreur est survenue'.failure(context);
+                          }
+                        },
+                        textColor: Theme.of(context).colorScheme.onError,
+                        backgroundColor: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                    Dimensions.xxs(context).spaceW(),
+                    Expanded(
+                      child: GypseElevatedButton(
+                        context,
+                        label: 'Annuler',
+                        onPressed: () => Navigator.pop(context),
+                        textColor: Theme.of(context).colorScheme.primary,
+                        backgroundColor: Theme.of(context)
+                            .colorScheme
+                            .surface
+                            .withOpacity(0.2),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
--- lib/home/presentation/views/widgets/profile/widgets/user_credentials_view.dart
+++ lib/home/presentation/views/widgets/profile/widgets/user_credentials_view.dart
@@ -1,12 +1,135 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gypse/auth/domain/usecase/auth_use_cases.dart';
+import 'package:gypse/auth/presentation/models/ui_user.dart';
+import 'package:gypse/common/providers/user_provider.dart';
+import 'package:gypse/common/style/buttons.dart';
+import 'package:gypse/common/style/fonts.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/enums.dart';
+import 'package:gypse/common/utils/extensions.dart';
+import 'package:gypse/common/utils/strings.dart';
+import 'package:gypse/home/presentation/views/widgets/profile/dialogs/delete_account_dialog.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class UserCredentialsView extends HookConsumerWidget {
-  const UserCredentialsView({super.key});
+  late UiUser? user;
+
+  UserCredentialsView({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: Text('UserCredentialsView'),
+    user = ref.watch(userProvider);
+    Future<bool> changePasswordUseCase() =>
+        ref.read(changePasswordUseCaseProvider).invoke();
+    Future<bool> signOutUseCase() => ref.read(signOutUseCaseProvider).invoke();
+
+    return ListView.separated(
+      itemCount: 5,
+      physics: NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.only(
+        top: Dimensions.s(context).height,
+        left: Dimensions.xs(context).width,
+        right: Dimensions.xs(context).width,
+      ),
+      separatorBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return Dimensions.s(context).spaceH();
+          case 3:
+            return Dimensions.s(context).spaceH();
+          default:
+            return Dimensions.xxs(context).spaceH();
+        }
+      },
+      itemBuilder: (context, index) => [
+        Image.asset(
+          '$imagesPath/splash_logo.png',
+          height: Dimensions.s(context).height,
+        ),
+        TextFormField(
+          enabled: false,
+          style: GypseFont.s(),
+          initialValue: user!.userName,
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+              labelText: 'Nom d\'utilisateur',
+              suffix: Icon(
+                Icons.person_outline,
+                color: Theme.of(context).colorScheme.onPrimary,
+              )),
+        ),
+        TextFormField(
+          enabled: false,
+          style: GypseFont.s(),
+          initialValue: user!.credentials.email,
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+              labelText: 'Adresse mail',
+              suffix: Icon(
+                Icons.alternate_email_outlined,
+                color: Theme.of(context).colorScheme.onPrimary,
+              )),
+        ),
+        InkWell(
+          onTap: () async {
+            bool result =
+                await changePasswordUseCase().onError((error, stackTrace) {
+              Navigator.pop(context);
+              return false;
+            });
+
+            if (result) {
+              'Un mail vous a été envoyé'.success(context);
+            } else {
+              'Une erreur est survenue'.failure(context);
+            }
+          },
+          child: TextFormField(
+            enabled: false,
+            style: GypseFont.s(color: Theme.of(context).colorScheme.secondary),
+            initialValue: 'Changer de mot de passe',
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                suffix: Icon(
+                  Icons.lock_outline,
+                  color: Theme.of(context).colorScheme.onPrimary,
+                )),
+          ),
+        ),
+        Row(children: [
+          Expanded(
+            child: GypseElevatedButton(
+              context,
+              onPressed: () => DeleteAccountDialog(context),
+              label: 'Supression',
+              textColor: Theme.of(context).colorScheme.secondary,
+              backgroundColor:
+                  Theme.of(context).colorScheme.surface.withOpacity(0.2),
+            ),
+          ),
+          Dimensions.xs(context).spaceW(),
+          Expanded(
+            child: GypseElevatedButton(
+              context,
+              onPressed: () async {
+                bool result = await signOutUseCase();
+                if (result) {
+                  context.go(Screen.authView.path);
+                } else {
+                  'Une erreur est survenue'.failure(context);
+                }
+              },
+              label: 'Déconnexion',
+              textColor: Theme.of(context).colorScheme.onPrimary,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ]),
+      ][index],
     );
   }
 }

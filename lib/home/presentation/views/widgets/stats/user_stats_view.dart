521e5afc0ec978a916a31ac8e2e6ca9b07108b4d
Menelick Grandisson <mgrandisson@exomind.fr>
2023-07-21 16:30:53 +0200

    FIX : prevent no data on stats

--- lib/home/presentation/views/widgets/stats/user_stats_view.dart
+++ lib/home/presentation/views/widgets/stats/user_stats_view.dart
@@ -1,16 +1,29 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
+import 'package:gypse/auth/presentation/models/ui_user.dart';
 import 'package:gypse/common/analytics/domain/usecase/firebase_analytics_use_cases.dart';
+import 'package:gypse/common/providers/user_provider.dart';
 import 'package:gypse/common/utils/dimensions.dart';
 import 'package:gypse/common/utils/enums.dart';
 import 'package:gypse/common/utils/extensions.dart';
+import 'package:gypse/home/presentation/views/widgets/dialogs/no_data_dialog.dart';
 import 'package:gypse/home/presentation/views/widgets/stats/widgets/gauge_chart_stats.dart';
 import 'package:gypse/home/presentation/views/widgets/stats/widgets/pie_chart_stats.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class UserStatsView extends HookConsumerWidget {
-  const UserStatsView({super.key});
+  late UiUser user;
+
+  UserStatsView({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    user = ref.watch(userProvider)!;
+
+    if (user.questions.isEmpty) {
+      Future(() => NoDataDialog(context));
+    }
+
     ref
         .read(logDisplayUseCaseProvider)
         .invoke(screen: Screen.homeView.path, details: 'Stats');

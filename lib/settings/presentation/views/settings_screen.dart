f4af26ee978079ff96833c89876acc3049da8ff6
Menelick Grandisson <mgrandisson@exomind.fr>
2023-08-10 11:30:41 +0200

    FEAT [SETTINGS] : init settings view

--- lib/settings/presentation/views/settings_screen.dart
+++ lib/settings/presentation/views/settings_screen.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/enums.dart';
+import 'package:gypse/common/utils/strings.dart';
 import 'package:gypse/settings/presentation/views/widgets/settings_view.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SettingsScreen extends HookConsumerWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: SettingsView(),
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        context.go(Screen.homeView.path);
+        return false;
+      },
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('$imagesPath/home_bkg.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          padding: EdgeInsets.only(
+            top: Dimensions.s(context).height,
+            bottom: Dimensions.xxs(context).height,
+            left: Dimensions.xs(context).width,
+            right: Dimensions.xs(context).width,
+          ),
+          child: SettingsView(),
+        ),
+      ),
     );
   }
 }
--- lib/settings/presentation/views/widgets/settings_view.dart
+++ lib/settings/presentation/views/widgets/settings_view.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:gypse/common/style/fonts.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/extensions.dart';
+import 'package:gypse/common/utils/strings.dart';
 
-class SettingsView extends HookConsumerWidget {
+class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
-    @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text('SettingsView'),
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(
+          '$imagesPath/splash_logo.png',
+          height: Dimensions.l(context).width,
+        ),
+        Dimensions.s(context).spaceH(),
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {},
+            splashColor: Theme.of(context).colorScheme.secondary,
+            child: TextFormField(
+              enabled: false,
+              style:
+                  GypseFont.s(color: Theme.of(context).colorScheme.secondary),
+              initialValue: 'Paramètres de jeu',
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  suffix: Icon(
+                Icons.casino_outlined,
+                color: Theme.of(context).colorScheme.onPrimary,
+              )),
+            ),
+          ),
+        ),
+        Dimensions.xxs(context).spaceH(),
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {},
+            splashColor: Theme.of(context).colorScheme.secondary,
+            child: TextFormField(
+              enabled: false,
+              style:
+                  GypseFont.s(color: Theme.of(context).colorScheme.secondary),
+              initialValue: 'Voir le profil',
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  suffix: Icon(
+                Icons.person_outline,
+                color: Theme.of(context).colorScheme.onPrimary,
+              )),
+            ),
+          ),
+        ),
+        Dimensions.xxs(context).spaceH(),
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {},
+            splashColor: Theme.of(context).colorScheme.secondary,
+            child: TextFormField(
+              enabled: false,
+              style:
+                  GypseFont.s(color: Theme.of(context).colorScheme.secondary),
+              initialValue: 'À propos de GYPSE',
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  suffix: Icon(
+                Icons.info_outline,
+                color: Theme.of(context).colorScheme.onPrimary,
+              )),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Text(
+              'version de l\'app: 1.2.0',
+              style: GypseFont.xs(),
+            ),
+          ),
+        )
+      ],
     );
-  }}
+  }
+}

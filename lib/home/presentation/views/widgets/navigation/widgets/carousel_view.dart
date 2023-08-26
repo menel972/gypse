af5084108bba7fb83868c2e91c72c2381a8e7776
Menelick Grandisson <mgrandisson@exomind.fr>
2023-08-27 00:19:00 +0200

    FIX [CAROUSEL] : fix no question book in carousel

--- lib/home/presentation/views/widgets/navigation/widgets/carousel_view.dart
+++ lib/home/presentation/views/widgets/navigation/widgets/carousel_view.dart
@@ -2,39 +2,25 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:gypse/common/providers/questions_provider.dart';
 import 'package:gypse/common/style/cards.dart';
 import 'package:gypse/common/utils/dimensions.dart';
 import 'package:gypse/common/utils/enums.dart';
+import 'package:gypse/game/presentation/models/ui_question.dart';
+import 'package:gypse/home/presentation/views/widgets/navigation/states/carousel_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CarouselView extends StatefulHookConsumerWidget {
+class CarouselView extends HookConsumerWidget {
   CarouselView({super.key});
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CarouselViewState();
-
-  List<Books> fetchFiveRandomBooks() {
-    List<Books> books = [...Books.values];
-    books.shuffle();
-    return books.take(5).toList();
-  }
-}
-
-class _CarouselViewState extends ConsumerState<CarouselView> {
   late List<Books> books;
-  final CarouselController carouselController = CarouselController();
+  late List<UiQuestion> questions;
 
   @override
-  void initState() {
-    List<Books> allBooks = [...Books.values];
-    allBooks.shuffle();
+  Widget build(BuildContext context, WidgetRef ref) {
+    questions = ref.watch(questionsProvider).toList();
+    books = ref.watch(carouselStateNotifierProvider(questions));
 
-    books = allBooks.take(5).toList();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return FlutterCarousel.builder(
       itemCount: 5,
       itemBuilder: (context, index, realIndex) => HomeCarouselCard(
@@ -42,7 +28,7 @@ class _CarouselViewState extends ConsumerState<CarouselView> {
         book: books[index],
         ref: ref,
       ),
-      options: Options(context, carouselController),
+      options: Options(context),
     );
   }
 }
@@ -50,9 +36,8 @@ class _CarouselViewState extends ConsumerState<CarouselView> {
 /// Extends [CarouselOptions] to provides parameters for [FlutterCarousel]
 class Options extends CarouselOptions {
   final BuildContext context;
-  final CarouselController carouselController;
 
-  Options(this.context, this.carouselController);
+  Options(this.context);
 
   @override
   double? get height => Dimensions.xl(context).height;
@@ -77,7 +62,4 @@ class Options extends CarouselOptions {
 
   @override
   Duration get autoPlayAnimationDuration => const Duration(milliseconds: 600);
-
-  @override
-  CarouselController? get controller => carouselController;
 }

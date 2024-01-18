981b040bb0d1f9991d7c460513976383ddab137c
Ménélick <mgrandisson@cegid.com>
2024-01-18 16:24:41 +0100

    FEAT [DIALOG] : create no auth dialog + generic dialog class

---
+++ lib/auth/presentation/views/widgets/sign_up/no_auth_dialog.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:gypse/common/style/buttons.dart';
+import 'package:gypse/common/style/fonts.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/extensions.dart';
+
+class NoAuthDialog extends Column {
+  final BuildContext context;
+  const NoAuthDialog(this.context, {super.key});
+
+  @override
+  MainAxisAlignment get mainAxisAlignment => MainAxisAlignment.center;
+
+  @override
+  CrossAxisAlignment get crossAxisAlignment => CrossAxisAlignment.stretch;
+
+  @override
+  List<Widget> get children => [
+        Text(
+          'Bienvenue sur Gypse !',
+          style: GypseFont.l(
+            bold: true,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xs(context).spaceH(),
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: 'Tu es sur le point d\'essayer Gypse ',
+                style:
+                    GypseFont.s(color: Theme.of(context).colorScheme.primary),
+              ),
+              TextSpan(
+                text: 'sans créer de compte.',
+                style: GypseFont.s(
+                    color: Theme.of(context).colorScheme.primary, bold: true),
+              ),
+            ],
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xs(context).spaceH(),
+        Text(
+          '⚠️ Certaines fonctionnalités ne te seront pas disponibles :',
+          style: GypseFont.s(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xxs(context).spaceH(),
+        Text(
+          '• Statistiques de tes performances.',
+          style: GypseFont.s(color: Theme.of(context).colorScheme.primary),
+          textAlign: TextAlign.center,
+        ),
+        Text(
+          '• Récapitulatif de tes parties.',
+          style: GypseFont.s(color: Theme.of(context).colorScheme.primary),
+          textAlign: TextAlign.center,
+        ),
+        Text(
+          '• Modification de la difficulté.',
+          style: GypseFont.s(color: Theme.of(context).colorScheme.primary),
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xs(context).spaceH(),
+        GypseElevatedButton(
+          context,
+          onPressed: () {},
+          label: 'Essayer Gypse !',
+          textColor: Theme.of(context).colorScheme.onPrimary,
+        ),
+        Dimensions.xxs(context).spaceH(),
+        GestureDetector(
+          onTap: () => Navigator.pop(context),
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text:
+                      'Pour pleinement profiter de l\'expérience Gypse, on te recommande de',
+                  style: GypseFont.xs(
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+                TextSpan(
+                  text: ' créer un compte.',
+                  style: GypseFont.xs(
+                    color: Theme.of(context).colorScheme.secondary,
+                    bold: true,
+                  ),
+                ),
+              ],
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ];
+}
---
+++ lib/common/style/dialogs.dart
@@ -0,0 +1,51 @@
+import 'package:blur/blur.dart';
+import 'package:flutter/material.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/extensions.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class GypseDialog extends HookConsumerWidget {
+  final BuildContext context;
+  final Widget child;
+  final double? height;
+  final bool dismissible;
+  final Function? onDismiss;
+
+  GypseDialog(
+    this.context, {
+    required this.child,
+    this.height,
+    this.dismissible = true,
+    this.onDismiss,
+    super.key,
+  }) {
+    showDialog(context: context, builder: (context) => this);
+  }
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return WillPopScope(
+      onWillPop: () async {
+        onDismiss?.call();
+        return dismissible;
+      },
+      child: Center(
+        child: Container(
+          height: height ?? Dimensions.xxl(context).height,
+          width: Dimensions.screen(context).width * 0.9,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface.withOpacity(0.4),
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Theme.of(context).colorScheme.surface),
+          ),
+          child: Blur(
+            blur: 3,
+            blurColor: Theme.of(context).colorScheme.surface,
+            borderRadius: BorderRadius.circular(20),
+            overlay: Dimensions.xxs(context).padding(child),
+            child: Container(),
+          ),
+        ),
+      ),
+    );
+  }
+}

e9440826b24a95d10ed451251ce0e4b8924726e1
Ménélick <mgrandisson@cegid.com>
2024-01-18 18:49:30 +0100

    FEAT [DIALOG] : create anonymous restriction dialog

---
+++ lib/common/style/anonymous_dialogs.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:gypse/common/style/buttons.dart';
+import 'package:gypse/common/style/fonts.dart';
+import 'package:gypse/common/utils/dimensions.dart';
+import 'package:gypse/common/utils/extensions.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AnonymousDenied extends HookConsumerWidget {
+  final String feature;
+
+  const AnonymousDenied(this.feature, {super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          'Débloque toutes les fonctionnalités de Gypse !',
+          style: GypseFont.l(
+            bold: true,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xs(context).spaceH(),
+        Text.rich(
+          TextSpan(children: [
+            TextSpan(
+              text: feature,
+              style: GypseFont.s(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            TextSpan(
+              text: ' aux utilisateurs connectés.',
+              style: GypseFont.s(
+                color: Theme.of(context).colorScheme.primary,
+                bold: true,
+              ),
+            ),
+          ]),
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xxs(context).spaceH(),
+        Text(
+          'Connecte-toi ou crée un compte pour en profiter !',
+          style: GypseFont.s(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xs(context).spaceH(),
+        GypseElevatedButton(
+          context,
+          onPressed: () {},
+          label: 'Je me connecte',
+          textColor: Theme.of(context).colorScheme.onPrimary,
+        ),
+      ],
+    );
+  }
+}

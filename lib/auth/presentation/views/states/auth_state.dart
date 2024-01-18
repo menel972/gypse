35d472493916c9c7b7fd88272a52d874ef6cc73b
Ménélick <mgrandisson@cegid.com>
2024-01-18 16:23:55 +0100

    REFACT [AUTH] : launch app on sign up

--- lib/auth/presentation/views/states/auth_state.dart
+++ lib/auth/presentation/views/states/auth_state.dart
@@ -1,7 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AuthStateNotifier extends StateNotifier<int> {
-  AuthStateNotifier() : super(1);
+  AuthStateNotifier() : super(0);
 
   void onViewChanged(int selectedView) => state = selectedView;
 }

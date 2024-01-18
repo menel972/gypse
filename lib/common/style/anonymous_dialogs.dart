b8def020390d2670c58a82e659a4b2642571e55b
Ménélick <mgrandisson@cegid.com>
2024-01-18 19:25:43 +0100

    FEAT [DIALOG] : create anonymous migration dialog

--- lib/common/style/anonymous_dialogs.dart
+++ lib/common/style/anonymous_dialogs.dart
@@ -64,3 +64,61 @@ class AnonymousDenied extends HookConsumerWidget {
     );
   }
 }
+
+class AnonymousMigration extends HookConsumerWidget {
+  const AnonymousMigration({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          'Tu aimes Gypse ?',
+          style: GypseFont.l(
+            bold: true,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xs(context).spaceH(),
+        Text(
+          'Approfondi ton expérience grâce aux fonctionnalités exclusives aux utilisateurs connectés :',
+          style: GypseFont.s(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xxs(context).spaceH(),
+        ...[
+          'Des statistiques de tes performances.',
+          'Un Récap de tes parties.',
+          'Plus de challenge en adaptant la difficulté.'
+        ].map(
+          (String feature) => Text(
+            '• $feature',
+            style: GypseFont.s(color: Theme.of(context).colorScheme.primary),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Dimensions.xxs(context).spaceH(),
+        Text(
+          'Connecte-toi ou crée un compte pour en profiter !',
+          style: GypseFont.s(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Dimensions.xs(context).spaceH(),
+        GypseElevatedButton(
+          context,
+          onPressed: () {},
+          label: 'Je me connecte',
+          textColor: Theme.of(context).colorScheme.onPrimary,
+        ),
+      ],
+    );
+  }
+}

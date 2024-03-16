0af1149e349eef815939dbfbb1df4d1601a1dfc1
Ménélick <menel972>
2024-03-16 17:50:15 +0100

    Revert "FEAT : IA"
    This reverts commit 61e7504758ae41fba4204f9a5aa1addcbb808638.

--- lib/home/presentation/views/widgets/navigation/home_view.dart
+++ lib/home/presentation/views/widgets/navigation/home_view.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gypse/common/analytics/domain/usecase/firebase_analytics_use_cases.dart';
 import 'package:gypse/common/style/buttons.dart';
@@ -9,7 +6,6 @@ import 'package:gypse/common/utils/dimensions.dart';
 import 'package:gypse/common/utils/enums/path_enum.dart';
 import 'package:gypse/common/utils/extensions.dart';
 import 'package:gypse/home/presentation/views/widgets/navigation/widgets/carousel_view.dart';
-import 'package:gypse/open_ai/open_ai_service.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomeView extends HookConsumerWidget {
@@ -28,11 +24,10 @@ class HomeView extends HookConsumerWidget {
           GypseButton.orange(
             context,
             onPressed: () {
-              OpenAiService().ask();
-              // ref
-              //     .read(logNavigationUseCaseProvider)
-              //     .invoke(from: Screen.homeView.path, to: Screen.gameView.path);
-              // context.go('${Screen.gameView.path}/ ');
+              ref
+                  .read(logNavigationUseCaseProvider)
+                  .invoke(from: Screen.homeView.path, to: Screen.gameView.path);
+              context.go('${Screen.gameView.path}/ ');
             },
             label: 'Mode aléatoire',
           ),
